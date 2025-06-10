3ce91eb031cf2a20f0852d6072a65db26be4dd57
AKASH TIWARI <akash9194@gmail.com>
2025-06-10 23:34:30 +0530

    redirect to login page if auth token is not present

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import React from "react";
+import Navbar from "./Navbar";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
-function Body() {
+const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector(store => store?.user);
+
+  const fetchUser = async () => {
+    if(user){
+      return;
+    }
+    try {
+      const result = await axios.get(BASE_URL + "profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(result.data));
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <>
-     <Navbar />
-     <Outlet/>
-     <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
-    
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
