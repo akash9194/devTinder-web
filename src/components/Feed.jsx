221ce27b8747127fe900e69fd5a34ca4c067d5b6
AKASH TIWARI <akash9194@gmail.com>
2025-06-11 20:24:47 +0530

    built feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { useEffect } from "react";
+import axios from "axios";
+import Usercard from "./Usercard";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
 
-export default Feed
+  const getFeed = async () => {
+    if (feed?.length) return;
+
+    try {
+      const resp = await axios.get(BASE_URL + "feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(resp.data));
+    } catch (error) {
+      // Error login may be redirect to error page
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+  return feed && (<div className="flex justify-center my-10">
+    <Usercard user={feed[0]}></Usercard>
+  </div>);
+};
+
+export default Feed;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 import { removeUser } from "../utils/userSlice";
+import { removeFeed } from "../utils/feedSlice";
 
 const Navbar = () => {
   const user = useSelector(store => store?.user);
@@ -16,6 +17,7 @@ const Navbar = () => {
           BASE_URL+ "logout", {}, {withCredentials: true}
         );
         dispatch(removeUser());
+        dispatch(removeFeed())
         return navigate("/login");
 
     } catch (error) {
